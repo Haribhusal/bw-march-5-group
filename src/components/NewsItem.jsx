89185e34d3b7ebfdfe642e5e70d26785f1c4a35a
virtualhari <hari@virtual-gravity.com>
2023-04-23 19:33:57 +0545

    fetched posts from api

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const NewsItem = ({ myTitle, imageUrl, desc, link }) => {
+const NewsItem = ({ myTitle, desc }) => {
   return (
-    <a href={link}>
+    <a href="/">
       <article className="post">
-        <figure>
-          <img src={imageUrl} alt="sasa" />
-        </figure>
+        {/* <figure><img src={imageUrl} alt="sasa" /></figure> */}
         <div className="info">
-          <h3>{myTitle}</h3>
-          <p>{desc}</p>
+          <h3>{myTitle.slice(0, 10)}...</h3>
+          <p>{desc.slice(0, 50)}...</p>
 
           <button>Read More</button>
         </div>
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 import newsData from "./../data/news.json";
+import axios from "axios";
 
 const NewsList = () => {
-  console.log(newsData);
+  const [fetchedPosts, setFetchedPost] = useState([]);
+
+  const fetchPosts = () =>
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setFetchedPost(res.data);
+    });
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // const [posts, setPosts] = useState([
+  //   {
+  //     id: 1,
+  //     title: "Hello 1",
+  //     desc: "Hello desc",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Hello 2",
+  //     desc: "Hello desc",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Hello 3",
+  //     desc: "Hello desc",
+  //   },
+  // ]);
+  // const [name, setName] = useState("");
+  // let name = "Hari";
+
+  // const changeName = () => {
+  //   name = " Ramesh";
+  // };
+  // changeName();
+
+  // console.log(newsData);
   return (
     <div className="newsList">
-      {newsData.map((item) => (
-        <NewsItem
-          myTitle={item.title}
-          imageUrl={item.imageLink}
-          desc={item.desc}
-          link={item.link}
-        />
+      {/* <h1>{name}</h1>
+      <button onClick={() => setName("Test Name")}>Change Name</button> */}
+      {/* {posts.map((item) => (
+        <h1>{item.title}</h1>
+      ))} */}
+
+      {fetchedPosts.map((item) => (
+        <NewsItem myTitle={item.title} desc={item.body} />
       ))}
     </div>
   );
